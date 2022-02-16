cc7fda213e568df76b11e80c46dfdad7cdab41df
Soul <1362880218@qq.com>
2022-02-16 21:26:09 +0800

    删除没用的东西

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Main from "./view/main/main";
+
+function App() {
+  return (
+    <Main />
+  );
+}
+
+export default App;
---
+++ src/view/main/main.js
@@ -0,0 +1,7 @@
+function Main() {
+  return (
+    <div>app</div>
+  )
+}
+
+export default Main
